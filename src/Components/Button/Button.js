396dd7508d7f778d2f7de471f3fbe6f814d9e0c7
Peyton Tanzillo <peyton.tanzillo@huskers.unl.edu>
2019-11-06 12:09:59 -0600

    Added buttons to expanded card that link to helpful resources

---
+++ src/Components/Button/Button.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './Button.css'
+
+import ThemeDataContext from 'Contexts/ThemeDataContext'
+
+import Hoverable from 'Components/Hoverable/Hoverable'
+
+import {setThemeColors} from 'Utilities/setThemeColors'
+
+class Button extends Hoverable {
+
+
+  render() {
+    const {button, variant} = this.props
+
+    const theme = this.context
+
+    const colors = setThemeColors(theme, ["buttonText", "buttonTextHover", "buttonBackground", "buttonBackgroundHover"])
+
+    const clickHandler = () => {
+      window.location.href = button.link
+    }
+
+
+    switch (variant) {
+      default:
+        return (
+            <div className="button" onClick={clickHandler}
+            style={{
+              color: this.state.hovered ? colors.buttonTextHover : colors.buttonText,
+              backgroundColor: this.state.hovered ? colors.buttonBackgroundHover : colors.buttonBackground,
+              boxShadow : this.state.hovered ? "" : theme.shadow
+            }}
+            onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover}>
+              {button.text}
+            </div>
+        );
+    }
+  }
+}
+Hoverable.contextType = ThemeDataContext
+
+export default Button;
--- src/Groups/ExpandedCard/ExpandedCard.js
+++ src/Groups/ExpandedCard/ExpandedCard.js
@@ -5,6 +5,7 @@ import './ExpandedCard.css';
 import ThemeDataContext from 'Contexts/ThemeDataContext'
 
 import Card from 'Groups/Card/Card'
+import Button from 'Components/Button/Button'
 
 import {setThemeColors} from 'Utilities/setThemeColors'
 
@@ -13,9 +14,20 @@ function ExpandedCard({
 }) {
   const theme = React.useContext(ThemeDataContext)
   const colors = setThemeColors(theme, ["cardBackground", "cardText"])
+
+  const cardButtons = []
+  if (card.buttons) {
+    for (const button of card.buttons) {
+      cardButtons.push(
+        <Button key={cardButtons.length} button={button} />
+      )
+    }
+  }
+
   return (
   <div className="card-expanded" style={{backgroundColor: colors.cardBackground}}>
     <Card card={card} variant="expanded"/>
+    <div className="button-container">{cardButtons}</div>
     <p className="card-description" style={{color: colors.cardText}}>
       {card.description}
     </p>
